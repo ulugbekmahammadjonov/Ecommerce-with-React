93f3e1fdf8142a93038b3abf54d408fd55e0f1d2
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-08-30 22:18:39 +0500

    lesson39

--- e-commerce/src/pages/Cart.js
+++ e-commerce/src/pages/Cart.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const Cart = () => {
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { PageHero } from "../components";
+const CartPage = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <main>
+      <PageHero title="cart" />
+      <Wrapper></Wrapper>
+    </main>
+  );
+};
 
-export default Cart
+const Wrapper = styled.main`
+  .empty {
+    text-align: center;
+    h2 {
+      margin-bottom: 1rem;
+      text-transform: none;
+    }
+  }
+`;
+
+export default CartPage;
---
+++ e-commerce/src/pages/CheckoutPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+const CheckoutPage = () => {
+  return <div>CheckoutPage</div>;
+};
+
+export default CheckoutPage;
