e6cfd0c77d415784ca3d9b562a88981d87afec1a
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-10-03 19:31:44 +0500

    lesson46

--- e-commerce/src/pages/index.js
+++ e-commerce/src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import About from "./About";
 import Cart from "./Cart";
-import Products from "./Products";
+import Products from "./ProductsPage";
 import Error from "./Error";
 export {Home, About, Products, Cart, Error}
